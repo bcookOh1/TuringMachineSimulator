0c1161125f1bef51ef0049992108985e18609db5
BennettCook <bcook845@live.com>
2021-12-27 22:05:23 -0500

    resize image

--- show_diagram/main.cpp
+++ show_diagram/main.cpp
@@ -3,19 +3,15 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Shared_Image.H>
 #include "MainWind.h"
+#include "CommonDefs.h"
 
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
-   string gvFile = "./img/BinaryAddition.gv";
-   if(argc > 1){
-      gvFile = argv[1]; 
-   } // end if 
-
    fl_register_images();
-   MainWind win(1126, 586, "Tm graph", gvFile);
+   MainWind win(MainWind_W_Max, MainWind_H_Max, "Tm graph");
    win.show(); 
    return(Fl::run());
 } // end main
